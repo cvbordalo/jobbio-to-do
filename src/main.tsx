bd4bda00c71496ae804d43acd3bcb90b3c9bbc54
Carlos Bordalo <carlosvitor6@hotmail.com>
2023-06-22 20:03:51 -0300

    feat: Cofigure Chakra UI and theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import { ChakraProvider } from '@chakra-ui/react';
+import ReactDOM from 'react-dom/client';
+import { App } from './App.tsx';
+import { theme } from './styles/theme.ts';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>
+);
---
+++ src/styles/theme.ts
@@ -0,0 +1,39 @@
+// 1. Import the extendTheme function
+import { extendTheme } from '@chakra-ui/react';
+
+export const theme = extendTheme({
+  colors: {
+    gray: {
+      '100': '#f2f2f2',
+      '200': '#d9d9d9',
+      '300': '#808080',
+      '400': '#333333',
+      '500': '#262626',
+      '600': '#1a1a1a',
+      '700': '#0d0d0d'
+    },
+    purple: {
+      '300': '#8284fa',
+      '700': '#5e60ce'
+    },
+    blue: {
+      '300': '#4ea8de',
+      '700': '#1e6f9f'
+    },
+    red: {
+      '700': '#e25858'
+    }
+  },
+  fonts: {
+    heading: 'Roboto',
+    body: 'Roboto'
+  },
+  styles: {
+    global: {
+      body: {
+        bg: 'gray.400',
+        color: 'gray.100'
+      }
+    }
+  }
+});
