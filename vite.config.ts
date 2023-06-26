5843c5ee6f49bf27216b5911dcfe85e83a99d6d4
Carlos Bordalo <carlosvitor6@hotmail.com>
2023-06-26 13:35:38 -0300

    config: Netlify test config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react-swc';
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()]
+// });
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true,
+    emptyOutDir: true,
+    manifest: true,
+    rollupOptions: {
+      output: {
+        entryFileNames: '[name].[hash].js',
+        chunkFileNames: '[name].[hash].js',
+        assetFileNames: '[name].[hash].[ext]'
+      }
+    }
+  }
 });
